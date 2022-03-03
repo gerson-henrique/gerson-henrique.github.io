1394f68e21eca6bebb09e07edcc8a7e05586fbdf
Gero <gerson_henrique@pop-os.localdomain>
2022-03-03 14:55:16 -0300

    feat : about section

--- src/components/header/HeaderSocials.js
+++ src/components/header/HeaderSocials.js
@@ -4,8 +4,8 @@ import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
 export default function HeaderSocials() {
   return (
     <div className='headersocials'>
-    <a href="https://www.linkedin.com/in/gerson-henrique/" target="_blank" > <AiFillLinkedin /> </a>
-    <a href="https://github.com/gerson-henrique/" target="_blank" > <AiFillGithub /> </a>
-    </div>
-  )
+     <a href="https://www.linkedin.com/in/gerson-henrique/" target="_blank" rel='noreferrer'> <AiFillLinkedin /> </a>
+     <a href="https://github.com/gerson-henrique/" target="_blank" rel='noreferrer' > <AiFillGithub /> </a>
+    </div> )
 }
+
